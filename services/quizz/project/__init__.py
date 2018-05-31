36ea90e3095379b0e0c48f610a9d21b78f4d1e43
marattm <mtulepbayevmonnie@hawk.iit.edu>
2018-05-31 01:51:03 -0500

    DB postgres Setup

--- services/quizz/project/__init__.py
+++ services/quizz/project/__init__.py
@@ -1,38 +1,37 @@
 # services/quizz/project/__init__.py
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the application
 app = Flask(__name__)
 
+# instantiate db
+db = SQLAlchemy(app)
+
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
 
+# model
+class User(db.Model):
+    __tablename__ = "high_score"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+    img = db.Column(db.String(128), nullable=False)
+    label_name = db.Column(db.String(128), nullable=False)
+    label_nb = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, img, label_name, label_nb):
+        self.username = username
+        self.email = email
+        self.label_name = label_name
+        self.label_nb = label_nb
+
+# route
 @app.route('/quizz/ping', methods=['GET'])
 def ping():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
-# services/quizz/project/__init__.py
-
-
-import os
-from flask import Flask, jsonify
-
-
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
-
-@app.route('/quizz/ping', methods=['GET'])
-def ping_pong():
     return jsonify({
         'status': 'success',
         'message': 'pong!'
---
+++ services/quizz/project/db/create.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE quizz_prod;
+CREATE DATABASE quizz_dev;
+CREATE DATABASE quizz_test;
